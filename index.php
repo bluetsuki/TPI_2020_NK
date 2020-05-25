471340b7e2397b1016acd3c2eceebe58584a8c2e
bluetsuki <bluetsuki7@gmail.com>
2020-05-25 13:17:11 +0200

    add structure website

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+$action = filter_input(INPUT_GET, "action", FILTER_SANITIZE_STRING);
+
+if(isset($_SESSION['role']))
+$role = $_SESSION['role'];
+else
+$role = "Nobody";
+
+$permission = [
+    "Nobody"=>[
+        "default" => "login",
+        "home" => "home",
+    ],
+    "Administrator" => [
+        "default" => "login",
+        "home" => "home",
+    ],
+    "Candidate" => [
+        "default" => "login",
+        "home" => "home",
+    ],
+    "Expert" => [
+        "default" => "login",
+        "home" => "home",
+    ],
+    "Manager" => [
+        "default" => "login",
+        "home" => "home",
+    ]
+];
+
+if (!array_key_exists($action, $permission[$role])) {
+    $action = "default";
+}
+
+try {
+    require './controller/'.$permission[$role][$action].'.php';
+} catch (Exception $e) {
+
+}
